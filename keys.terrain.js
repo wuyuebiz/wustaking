3b6e6974f67a98a08fd0df049ec3cae4ff67c934
Wu Yue <wuyuebiz@gmail.com>
2022-03-06 05:37:52 -0800

    init

---
+++ keys.terrain.js
@@ -0,0 +1,16 @@
+// can use `process.env.SECRET_MNEMONIC` or `process.env.SECRET_PRIV_KEY`
+// to populate secret in CI environment instead of hardcoding
+
+module.exports = {
+  custom_tester_1: {
+    mnemonic:
+      "shiver position copy catalog upset verify cheap library enjoy extend second peasant basic kit polar business document shrug pass chuckle lottery blind ecology stand",
+  },
+  custom_tester_2: {
+    privateKey: "fGl1yNoUnnNUqTUXXhxH9vJU0htlz9lWwBt3fQw+ixw=",
+  },
+  bombay: {
+    mnemonic:
+      "caution broken coach company resource rack monkey stage flame shaft treat mixed flight ski canyon suffer finger board odor cigar artefact jacket leg acoustic",
+  },
+};
